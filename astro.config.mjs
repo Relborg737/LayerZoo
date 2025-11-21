dd3d7cb7df6423364021f05b8955f59a7e8b71a6
Relborg737 <Relborg737@users.noreply.github.com>
2025-11-21 13:28:36 +0000

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/static';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'static',
-  adapter: vercel()
+  output: 'static'
 });
